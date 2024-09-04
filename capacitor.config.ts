d1460cc58aa6f6db4300237adc9579740dd8c76e
echelr73 <eche_lr73@hotmail.com>
2024-09-04 11:44:22 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Burger_Queen',
+  webDir: 'www'
+};
+
+export default config;
